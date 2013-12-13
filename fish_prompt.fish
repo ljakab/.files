a98195f76967f0cd8962c255fef6c953cf7a285a
Lorand Jakab <lojakab@cisco.com>
2013-12-13 14:56:17 +0200

    fish: add git prompt

---
+++ fish_prompt.fish
@@ -0,0 +1,50 @@
+# Place this file into $HOME/.config/fish/functions
+# initial author: Mariusz Smykula <mariuszs at gmail.com>
+# some improvements by Lori Jakab
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "+"
+set -g __fish_git_prompt_char_dirtystate "*"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green bold
+
+
+function fish_prompt --description 'Write out the prompt'
+
+	set -l last_status $status
+	
+	# Just calculate these once, to save a few cycles when displaying the prompt
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	end
+
+	if not set -q __fish_prompt_normal
+		set -g __fish_prompt_normal (set_color normal)
+	end
+
+	if not set -q __fish_prompt_cwd
+		set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+	end
+
+	if not test $last_status -eq 0
+		set __fish_prompt_error (set_color $fish_color_error)
+	end
+
+	echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" (printf '%s' (__fish_git_prompt)) "$__fish_prompt_error" '> ' "$__fish_prompt_normal"
+
+end
