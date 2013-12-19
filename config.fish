59c620532c4b2bfe183c3a81614c72d2d9c89c81
Lorand Jakab <lojakab@cisco.com>
2013-12-19 16:27:50 +0200

    fish: add configuration file

---
+++ config.fish
@@ -0,0 +1,11 @@
+# Disable greeting text when launching
+set -g -x fish_greeting ''
+
+# Prepend some additional directories to $PATH
+set -g -x fish_user_paths $HOME/bin /usr/local/sbin /usr/sbin /sbin
+
+# Set Maven options so ODL builds successfully
+set -g -x MAVEN_OPTS -Xmx1024m -XX:MaxPermSize=256m
+
+# Set default Map-Resolver for lig
+set -g -x LISP_MAP_RESOLVER l3-london-mr-ms.rloc.lisp4.net
