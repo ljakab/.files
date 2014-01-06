0474952797a5761e93f2b8502914455e1c9edb94
Lorand Jakab <lojakab@cisco.com>
2014-01-06 23:01:12 +0200

    fish: config: fix MAVEN_OPTS env variable

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ set -g -x fish_greeting ''
 set -g -x fish_user_paths $HOME/bin /usr/local/sbin /usr/sbin /sbin
 
 # Set Maven options so ODL builds successfully
-set -g -x MAVEN_OPTS -Xmx1024m -XX:MaxPermSize=256m
+set -g -x MAVEN_OPTS "-Xmx1024m -XX:MaxPermSize=256m"
 
 # Set default Map-Resolver for lig
 set -g -x LISP_MAP_RESOLVER l3-london-mr-ms.rloc.lisp4.net
