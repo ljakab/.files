fe82c3459ef15994fc9fb691926e745cc3e089a1
Lorand Jakab <lojakab@cisco.com>
2019-01-04 10:34:06 +0100

    fish: set LANG variable

--- config.fish
+++ config.fish
@@ -4,6 +4,9 @@ set -g -x fish_greeting ''
 # Prepend some additional directories to $PATH
 set -g -x fish_user_paths $HOME/bin /usr/local/sbin /usr/sbin /sbin
 
+# Set language
+set -g -x LANG en_US.UTF-8
+
 # Source $HOME/.fishrc if it exists and we're interactive
 if status --is-interactive;
     [ -e $HOME/.fishrc ]; and source $HOME/.fishrc
