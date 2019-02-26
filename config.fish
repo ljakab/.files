9eab6e62a15daf40eaea31be8715f951313f3733
Lorand Jakab <lojakab@cisco.com>
2019-02-26 10:47:46 +0100

    fish: cd $HOME if interactive

--- config.fish
+++ config.fish
@@ -2,12 +2,13 @@
 set -g -x fish_greeting ''
 
 # Prepend some additional directories to $PATH
-set -g -x fish_user_paths $HOME/bin /usr/local/sbin /usr/sbin /sbin
+set -g -x fish_user_paths $HOME/bin $HOME/node_modules/.bin /usr/local/sbin /usr/sbin /sbin
 
 # Set language
 set -g -x LANG en_US.UTF-8
 
 # Source $HOME/.fishrc if it exists and we're interactive
 if status --is-interactive;
+    cd $HOME
     [ -e $HOME/.fishrc ]; and source $HOME/.fishrc
 end
