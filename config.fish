702974554cc0358988adb07c51ad81fff5c8af97
Lori Jakab <lojakab@cisco.com>
2020-11-26 17:30:42 +0100

    fish: do NOT cwd to $HOME on startup!

--- config.fish
+++ config.fish
@@ -9,6 +9,5 @@ set -g -x LANG en_US.UTF-8
 
 # Source $HOME/.fishrc if it exists and we're interactive
 if status --is-interactive;
-    cd $HOME
     [ -e $HOME/.fishrc ]; and source $HOME/.fishrc
 end
